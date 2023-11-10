154cb21d512b12901b8ef3e316cd396ac2af7fb5
GitHub Action <action@github.com>
2023-11-10 05:40:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/laasaby/demo/MyBundle.kt
+++ src/main/kotlin/com/github/laasaby/demo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.laasaby.demo
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/laasaby/demo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/laasaby/demo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.laasaby.demo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.laasaby.demo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
